73960e01d0f2da4ebd8d835b32aac27c844b1edd
Ashley Wysocki <ashleygwysocki@gmail.com>
2012-11-26 21:30:16 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pandemic::Application.config.secret_token = '871a5b57716be6bb670de5e60376570958773f120e53718a1093417c036b79bd6e518321c6790ce8883f074e6e20c66d981b6d65259684ff76e63bf09327ca95'
